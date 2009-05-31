88252af75478178cf0b64c592daabc6476e5c45f
Tomasz Mazur <mazzy@trix.pl>
2009-05-31 14:48:25 +0200

    quoting table_name

--- lib/trixy_scopes.rb
+++ lib/trixy_scopes.rb
@@ -13,58 +13,59 @@ module TrixyScopes
       named_scope :not_in, lambda { |*ids| { :conditions => ["#{table_name}.`id` NOT IN(?)", ids.flatten] } }
       
       klass.columns.each do |column|
-        quoted_column_name = ActiveRecord::Base.connection.quote_table_name("#{klass.table_name}.#{column.name}")
-  
-        named_scope "#{column.name}_is", lambda { |value| { :conditions => { column.name => value } } }
-        named_scope "#{column.name}_is_not", lambda { |value| { :conditions => ["#{quoted_column_name} != ?", value] } }
-        named_scope "#{column.name}_is_nil", :conditions => { column.name => nil }
-        named_scope "#{column.name}_is_not_nil", :conditions => "#{quoted_column_name} IS NOT NULL"
+        attribute = column.name
+        quoted_column_name = ActiveRecord::Base.connection.quote_table_name("#{klass.table_name}.#{attribute}")
+        
+        named_scope "#{attribute}_is", lambda { |value| { :conditions => { attribute => value } } }
+        named_scope "#{attribute}_is_not", lambda { |value| { :conditions => ["#{quoted_column_name} != ?", value] } }
+        named_scope "#{attribute}_is_nil", :conditions => { attribute => nil }
+        named_scope "#{attribute}_is_not_nil", :conditions => "#{quoted_column_name} IS NOT NULL"
         
         unless column.type == :boolean
-          named_scope "#{column.name}_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} BETWEEN ? AND ?", from, to] } }
-          named_scope "#{column.name}_not_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} NOT BETWEEN ? AND ?", from, to] } }
+          named_scope "#{attribute}_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} BETWEEN ? AND ?", from, to] } }
+          named_scope "#{attribute}_not_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} NOT BETWEEN ? AND ?", from, to] } }
         end
         
         unless column.type == :boolean || column.type == :text
-          named_scope "#{column.name}_in", lambda { |*elements| { :conditions => { column.name => elements.flatten } } }
-          named_scope "#{column.name}_not_in", lambda { |*elements| { :conditions => ["#{quoted_column_name} NOT IN(?)", elements.flatten] } }
+          named_scope "#{attribute}_in", lambda { |*elements| { :conditions => { attribute => elements.flatten } } }
+          named_scope "#{attribute}_not_in", lambda { |*elements| { :conditions => ["#{quoted_column_name} NOT IN(?)", elements.flatten] } }
         end
                 
         if column.type == :datetime
-          named_scope "#{column.name}_before", lambda { |datetime| { :conditions => ["#{quoted_column_name} < ?", datetime] } }
-          named_scope "#{column.name}_after", lambda { |datetime| { :conditions => ["#{quoted_column_name} > ?", datetime] } }
+          named_scope "#{attribute}_before", lambda { |datetime| { :conditions => ["#{quoted_column_name} < ?", datetime] } }
+          named_scope "#{attribute}_after", lambda { |datetime| { :conditions => ["#{quoted_column_name} > ?", datetime] } }
           
-          if column.name.last(3) == "_at"
-            column_name_alias = column.name.chomp("_at")
-            named_scope "#{column_name_alias}_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} BETWEEN ? AND ?", from, to] } }
-            named_scope "#{column_name_alias}_not_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} NOT BETWEEN ? AND ?", from, to] } }
-            named_scope "#{column_name_alias}_before", lambda { |datetime| { :conditions => ["#{quoted_column_name} < ?", datetime] } }
-            named_scope "#{column_name_alias}_after", lambda { |datetime| { :conditions => ["#{quoted_column_name} > ?", datetime] } }
+          if attribute.last(3) == "_at"
+            attribute_alias = attribute.chomp("_at")
+            named_scope "#{attribute_alias}_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} BETWEEN ? AND ?", from, to] } }
+            named_scope "#{attribute_alias}_not_between", lambda { |from, to| { :conditions => ["#{quoted_column_name} NOT BETWEEN ? AND ?", from, to] } }
+            named_scope "#{attribute_alias}_before", lambda { |datetime| { :conditions => ["#{quoted_column_name} < ?", datetime] } }
+            named_scope "#{attribute_alias}_after", lambda { |datetime| { :conditions => ["#{quoted_column_name} > ?", datetime] } }
           end
         end
         
         if column.type == :integer || column.type == :float
-          named_scope "#{column.name}_greater_than", lambda { |value| { :conditions => ["#{quoted_column_name} > ?", value] } }
-          named_scope "#{column.name}_greater_or_equal_to", lambda { |value| { :conditions => ["#{quoted_column_name} >= ?", value] } }
-          named_scope "#{column.name}_less_than", lambda { |value| { :conditions => ["#{quoted_column_name} < ?", value] } }
-          named_scope "#{column.name}_less_or_equal_to", lambda { |value| { :conditions => ["#{quoted_column_name} <= ?", value] } }
+          named_scope "#{attribute}_greater_than", lambda { |value| { :conditions => ["#{quoted_column_name} > ?", value] } }
+          named_scope "#{attribute}_greater_or_equal_to", lambda { |value| { :conditions => ["#{quoted_column_name} >= ?", value] } }
+          named_scope "#{attribute}_less_than", lambda { |value| { :conditions => ["#{quoted_column_name} < ?", value] } }
+          named_scope "#{attribute}_less_or_equal_to", lambda { |value| { :conditions => ["#{quoted_column_name} <= ?", value] } }
         end
         
         if column.type == :string
-          named_scope "#{column.name}_starts_with", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", "#{string}%"] } }
-          named_scope "#{column.name}_ends_with", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", "%#{string}"] } }
-          named_scope "#{column.name}_includes", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", "%#{string}%"] } }
-          named_scope "#{column.name}_matches", lambda { |regexp| { :conditions => ["#{quoted_column_name} REGEXP ?", regexp] } }
+          named_scope "#{attribute}_starts_with", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", "#{string}%"] } }
+          named_scope "#{attribute}_ends_with", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", "%#{string}"] } }
+          named_scope "#{attribute}_includes", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", "%#{string}%"] } }
+          named_scope "#{attribute}_matches", lambda { |regexp| { :conditions => ["#{quoted_column_name} REGEXP ?", regexp] } }
         end
         
         if column.type == :text || column.type == :string
-          named_scope "#{column.name}_like", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", string] } }
-          named_scope "#{column.name}_not_like", lambda { |string| { :conditions => ["#{quoted_column_name} NOT LIKE ?", string] } }
+          named_scope "#{attribute}_like", lambda { |string| { :conditions => ["#{quoted_column_name} LIKE ?", string] } }
+          named_scope "#{attribute}_not_like", lambda { |string| { :conditions => ["#{quoted_column_name} NOT LIKE ?", string] } }
         end
         
         if column.type == :boolean
-          named_scope "#{column.name}", :conditions => { column.name => true }
-          named_scope "not_#{column.name}", :conditions => { column.name => false }
+          named_scope "#{attribute}", :conditions => { attribute => true }
+          named_scope "not_#{attribute}", :conditions => { attribute => false }
         end
       end
       
